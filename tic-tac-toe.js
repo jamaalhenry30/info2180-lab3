e367544eef0444e0fbf6c44c4f194dafd48d6803
Jamaal Henry <jamaalhenry30@gmail.com>
2021-10-10 13:46:46 -0500

    edited tic-tac-toe.js file to add X and O to each square only once

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,31 @@
-function  gridsetup(){
-let box = document.getElementById('board');
-let squared = box.getElementsByTagName('div');
-    for (var i = 0; i < squared.length; i++) {
-       squared[i].className = "square";
-    }
-let squares= document.getElementsByClassName('square');
-}
+window.onload=function(){
+        let box = document.getElementById('board');
+        let squares = box.getElementsByTagName('div');
+        let boardplay = [];
 
-window.onload = function(){
-    gridsetup(); 
-}
+        for (var i = 0; i < squares.length; i++) {
+            squares[i].className = "square";
+            squares[i].id=""+i;
+        }
+        var k = 0;
+        for(const box of squares){
+            box.addEventListener("click",function(){
+                console.log("square clicked");
+                if (k%2==0){
+                    box.innerHTML = "X";
+                    box.classList.add("X");
+                    boardplay.push(box.id);
+                    console.log(boardplay)
+                }
+                else{
+                    box.innerHTML = "O";
+                    box.classList.add("O");
+                    boardplay.push(box.id);
+                    console.log(boardplay)
 
+                }k++;
+
+            },{once: true});
+           
+            }
+}
